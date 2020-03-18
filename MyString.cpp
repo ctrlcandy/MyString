80b98465f2ac74374d366303058dfb1f5ce0830a
ctrlcandy <alexandraleshanu@yandex.ru>
2020-03-18 03:10:47 +0300

    Реализованы все методы

--- MyString.cpp
+++ MyString.cpp
@@ -23,6 +23,28 @@ MyString::MyString(const char* string) {
 	strcpy(_data, string);
 }
 
+MyString MyString::cat(const MyString& first) const {
+	MyString result;
+	result._size = this->_size + first._size;
+	strcat(result._data, this->_data);
+	strcat(result._data, first._data);
+
+	return result;
+}
+
+MyString& MyString::mutableCat(const MyString& first) {
+	char* buf = new char[this->_size + first._size + 1];
+	if (this->_data) {
+		strcpy(buf, this->_data);
+		delete[] this->_data;
+	}
+	strcat(buf, first._data);
+	this->_data = buf;
+	this->_size = this->_size + first._size;
+
+	return *this;
+}
+
 MyString& MyString::operator= (const MyString &string) {
 	MyString buf(string);
 	_size = buf._size;
@@ -40,50 +62,65 @@ MyString& MyString::operator= (const MyString &string) {
 }
 
 MyString& MyString::operator= (const char * string) {
-	/* Добавить вариант с использованием конструктора копирования */
+	/*
+	MyString buf(string);
+	_size = buf._size;
+	swap(_data, buf._data); 
+	*/
 
-	std::cout << "Copy = operator for cstring" << std::endl;
 	delete[] _data;
 	_size = strlen(string);
 	_data = new char[_size + 1];
 	strcpy(_data, string);
+
 	return *this;
 }
 
-MyString MyString::operator+ (MyString& string) {
-
+MyString MyString::operator+ (MyString& string) const {
+	return this->cat(string);
 }
 
-MyString MyString::operator+ (const char* string) { 
-
+MyString MyString::operator+ (const char* string) const { 
+	MyString tmp(string);
+	return this->cat(tmp);
 } 
-MyString& MyString::operator+= (MyString& string) {
 
+MyString& MyString::operator+= (const MyString& string)  {
+	mutableCat(string);
+	return *this;
 } 
 
 MyString& MyString::operator+= (const char* string) {
-
+	MyString tmp(string);
+	mutableCat(tmp);
+	return *this;
 } 
 
-bool MyString::operator== (MyString& string) {
-
+bool MyString::operator== (const MyString& string) const {
+	return strcmp(this->_data, string._data) == 0;
 } 
 
-bool MyString::operator== (const char* string) {
-
+bool MyString::operator== (const char* string) const {
+	return strcmp(this->_data, string) == 0;
 } 
 
 MyString MyString::reverse() {
+	MyString tmp(*this);
+	for (size_t i = 0; i < this->_size; i++) {
+		this->_data[i] = tmp._data[this->_size - 1 - i];
+	}
 
+	return *this;
 }
 
-unsigned MyString::size() {
+unsigned MyString::size() const {
 	return _size;
 }
 
-bool MyString::isEmpty() {
-	if (_size == 0)
-		return true;
-	else 
-		return false;
+bool MyString::isEmpty() const {
+	return _size == 0;
 }
+
+//void MyString::print() const {
+//	std::cout << this->_data << std::endl;
+//}
--- MyString.h
+++ MyString.h
@@ -9,25 +9,28 @@ using std::swap;
 class MyString
 {
 public:
-	MyString();										//конструктор по умолчанию
-	MyString(const MyString &string);				//конструктор копирования [MyString copyString(string);]
-	MyString(const char* string);					//конструктор копирования [MyString copyString(string);]
+	MyString();						//конструктор по умолчанию
+	MyString(const MyString &string);			//конструктор копирования [MyString copyString(string);]
+	MyString(const char* string);					
 
-	MyString& operator= (const MyString &string);	//оператор присваивания копированием
-	MyString& operator= (const char * string);		//оператор присваивания копированием
+	MyString cat(const MyString& first) const;
+	MyString& mutableCat(const MyString& first);
 
-	MyString operator+ (MyString &string);			//сложение
-	MyString operator+ (const char * string);		//сложение
+	MyString& operator= (const MyString &string);		//оператор присваивания копированием
+	MyString& operator= (const char* string);		//оператор присваивания копированием
 
-	MyString& operator+= (MyString& string);		//сложение и присваивание
-	MyString& operator+= (const char * string);		//сложение и присваивание
+	MyString operator+ (MyString &string) const;		//сложение
+	MyString operator+ (const char* string) const;		//сложение
 
-	bool	operator== (MyString& string);			//равенство строк
-	bool	operator== (const char* string);		//равенство строк
+	MyString& operator+= (const MyString& string);		//сложение и присваивание
+	MyString& operator+= (const char* string);		//сложение и присваивание
 
-	MyString	reverse();							//разворот строки
-	unsigned	size();								//размер
-	bool		isEmpty();							//проверка, пустая ли строка
+	bool	operator== (const MyString& string) const;	//равенство строк
+	bool	operator== (const char* string) const;		//равенство строк
+
+	MyString	reverse();				//разворот строки
+	unsigned	size() const;				//размер
+	bool		isEmpty() const;			//проверка, пустая ли строка
 
 
 	friend std::ostream& operator<< (std::ostream& stream, const MyString &string) {
@@ -35,15 +38,16 @@ public:
 		return stream;
 	}
 
-	friend std::istream& operator >> (std::istream& stream, const MyString &string) {
-		//дополнить код
+	friend std::istream& operator >> (std::istream& stream, MyString &string) {
+		stream >> string._data;
+		string._size = strlen(string._data);
 		return stream;
 	}
 
 	~MyString();									
 
 private:
-	char*			_data;
+	char*		_data;
 	unsigned int	_size;
 };
 
